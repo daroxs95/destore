68978472e607d624fa959b90c15efe4e14bef647
Darián López Utra <daroxs95@gmail.com>
2023-11-08 02:24:27 -0500

    Add WIP Tag controller and route

---
+++ app/Http/Controllers/TagController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Tag;
+
+class TagController extends Controller
+{
+    // Retrieve all tags
+    public function index()
+    {
+        $tags = Tag::all();
+        return response()->json($tags);
+    }
+
+    // Create a new tag
+    public function store(Request $request)
+    {
+        $tag = Tag::create($request->all());
+        return response()->json(['status' => 'Success', 'message' => 'Tag created', 'data' => $tag]);
+    }
+
+    // Retrieve a specific tag
+    public function show(Tag $tag)
+    {
+        return response()->json($tag);
+    }
+
+    // Update a tag
+    public function update(Request $request, Tag $tag)
+    {
+        $tag->update($request->all());
+        return response()->json(['status' => 'Success', 'message' => 'Tag updated']);
+    }
+
+    // Delete a tag
+    public function destroy(Tag $tag)
+    {
+        $tag->delete();
+        return response()->json(['status' => 'Success', 'message' => 'Tag deleted']);
+    }
+}
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -10,4 +10,9 @@ class Tag extends Model
     use HasFactory;
     protected $table = 'tags'; // Set the table name (adjust as needed)
     protected $fillable = ['name', 'description'];
+
+    public static $rules = [
+        'name' => 'required|unique:tags',
+        // Other validation rules for other fields (if any)
+    ];
 }

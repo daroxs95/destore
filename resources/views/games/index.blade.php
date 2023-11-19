bbedd3d6039393eaeea2971ac57294a3f7864e87
Darián López Utra <daroxs95@gmail.com>
2023-11-19 05:07:43 -0500

    Improve games grid styles
    - Remove view more link from card

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -2,25 +2,8 @@
     <main>
         <ul class="games-grid">
             @foreach($games as $game)
-
-                <li class="card cool-highlight">
-                    <h2 class="">{{$game->title}}</h2>
-                    <a href="{{route('games.showUI', ['game' => $game])}}" class="">
-                        View more
-                    </a>
-                </li>
+                <x-game-card :game="$game" />
             @endforeach
         </ul>
     </main>
-
-    <style>
-        .games-grid {
-            max-width: var(--max-width);
-            margin: auto;
-            display: grid;
-            grid-template-columns: repeat(auto-fill, minmax(var(--game-card-width), 1fr));
-            grid-gap: 1rem;
-            padding: 1rem;
-        }
-    </style>
 </x-site-layout>

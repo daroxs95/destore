087fae75478a905bb0d7d22986e9220ee16a9b9d
Darián López Utra <daroxs95@gmail.com>
2023-11-23 10:58:37 -0500

    Add game comments views
    - Allowing lazy loading temporarily

--- resources/views/games/show.blade.php
+++ resources/views/games/show.blade.php
@@ -15,11 +15,31 @@
         <div>
             {{__("Released in")}}: {{$game->release_date->formatLocalized('%B %e, %Y')}}
         </div>
-        <br />
-        <img class="game-details-main-img" src="{{$game->media->first()->getUrl('normal')}}" alt="" />
-        <br />
+        <br/>
+        <img class="game-details-main-img" src="{{$game->media->first()->getUrl('normal')}}" alt=""/>
+        <br/>
         <p>
             {{$game->description}}
         </p>
+
+        <h4>({{$game->comments->count()}}) {{__("Comments")}}</h4>
+        <x-game-comments :comments="$game->comments" :game_id="$game->id"/>
+        <hr/>
+        @auth
+            <br/>
+            <h4>{{__("Comment")}}</h4>
+            <form class="comments-container" method="post" action="{{ route('games.comments.store') }}">
+                @csrf
+                <div class="form-group">
+                    <textarea class="form-control" name="body"></textarea>
+                    <input type="hidden" name="game_id" value="{{ $game->id }}"/>
+                </div>
+                <div class="w-100">
+                    <button type="submit" class="btn btn-success">
+                        {{__("Add Comment")}}
+                    </button>
+                </div>
+            </form>
+        @endauth
     </main>
 </x-site-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\GameCommentController;
 use App\Http\Controllers\GameController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
@@ -18,16 +19,22 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [GameController::class, 'index'])
     ->name('index');
+
 Route::get('users', [UserController::class, 'index'])
     ->name('users.index')
     ->middleware(['auth', 'is.admin']);
 Route::get('users/{id}', [UserController::class, 'show'])
     ->middleware(['auth', 'verified'])
     ->name('users.show');
+
 Route::get('games', [GameController::class, 'index'])
     ->name('games.index');
 Route::get('games/{game:slug}', [GameController::class, 'show'])
     ->name('games.show');
+
+Route::post('games/comments', [GameCommentController::class, 'store'])
+    ->middleware(['auth', 'verified'])
+    ->name('games.comments.store');
 
 Route::post('/profile', [ProfileController::class, 'destroy'])
     ->middleware(['auth', 'verified'])

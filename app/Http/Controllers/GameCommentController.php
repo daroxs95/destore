de04f4dca9eb3101eea16e4b4daa49d8860ac1d2
Darián López Utra <daroxs95@gmail.com>
2024-01-19 09:34:47 +0100

    Add profanity filter to comments

--- app/Http/Controllers/GameCommentController.php
+++ app/Http/Controllers/GameCommentController.php
@@ -3,17 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Models\GameComment;
+use App\Services\ProfanityFilter;
 use Illuminate\Http\Request;
 
 class GameCommentController extends Controller
 {
     public function store(Request $request)
     {
+        $profanityFilter = new ProfanityFilter();
+
         $request->validate([
             'body' => 'required',
         ]);
         $input = $request->all();
         $input['user_id'] = auth()->user()->id;
+        $input['body'] = $profanityFilter->filter($input['body']);
         GameComment::create($input);
 
         return back();
